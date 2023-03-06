481a599c8b7b931a8419380996a0bf6d1dac5092
Luca Di Mascolo <lucadimascolo@users.noreply.github.com>
2023-03-06 11:08:49 +0100

    missing entry point

--- edukoi/__init__.py
+++ edukoi/__init__.py
@@ -1,7 +1,6 @@
 from .core import *
 
-
-def basic():
+def edukoi(show,renorm):
   pars = argparse.ArgumentParser()
   pars.add_argument('image',
                     help='Input image',
@@ -17,25 +16,8 @@ def basic():
                     default=2,metavar=('box'),type=float)
 
   args = pars.parse_args()
+  start(image=args.image,show=show,mode=args.mode,notes=(args.notes[0],args.notes[1],args.notes[2]),volume=0,box=args.box,renorm=renorm)
 
-  start(image=args.image,show=True,mode=args.mode,notes=(args.notes[0],args.notes[1],args.notes[2]),volume=0,box=args.box)
-
-
-def test():
-  pars = argparse.ArgumentParser()
-  pars.add_argument('image',
-                    help='Input image',
-                    nargs='?')
-  pars.add_argument('--notes',
-                    help='Define the pitch values (as note+octave format; e.g., C4)',
-                    nargs=3,default=['C3','C3','C3'],metavar=('red','green','blue'))
-  pars.add_argument('--mode',
-                    help='Select herakoi mode [single/adaptive/scan]',
-                    default='single',metavar=('mode'))
-  pars.add_argument('--box',
-                    help='sonification box size in units of frame percentage',
-                    default=2,metavar=('box'),type=float)
-
-  args = pars.parse_args()
-
-  start(image=args.image,show=False,mode=args.mode,notes=(args.notes[0],args.notes[1],args.notes[2]),volume=0,box=args.box)
+def basic(): edukoi(show =  True, renorm =  True)
+def  test(): edukoi(show = False, renorm =  True)
+def   art(): edukoi(show =  True, renorm = False)
